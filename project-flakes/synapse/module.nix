3f79dd583dfc617f11be37b6e47c66707188f9e6
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-10-21 01:13:20 +0200

    Fix typo

--- project-flakes/synapse/module.nix
+++ project-flakes/synapse/module.nix
@@ -54,7 +54,7 @@
   # Automatically activate the poetry virtualenv upon entering the shell.
   languages.python.poetry.activate.enable = true;
   # Install all extra Python dependencies; this is needed to run the unit
-  # tests and utilitise all Synapse features.
+  # tests and utilise all Synapse features.
   languages.python.poetry.install.arguments = ["--extras all"];
   # Install the 'matrix-synapse' package from the local checkout.
   languages.python.poetry.install.installRootPackage = true;
